84c2d899c6a93becbdd4420211208395a2574e53
gshavlik <gshavlik@unomaha.edu>
2024-08-28 16:41:40 +0000

    completed assignment

--- MadLib.py
+++ MadLib.py
@@ -6,9 +6,15 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("Enter a noun: ")
+  verb1 = input("Enter a verb in past tense: ")
+  adj1 = input("Enter an adjective that describes how you are doing the verb: ")
+  noun2 = input("Enter a second noun: ")
+  adj2 = input("Enter a second adjective to describe a feeling: ")
+  verb2 = input("Enter a second verb to describe what you're doing: ")
   #Print the story with the user supplied words.
-
+  print("I", adj1 , verb1 , "to the store to get" , noun1 , ".")
+  print("I also wanted to get" , noun2 , "but" , adj2 , "they were out so I" , verb2 , "home.")
 
 
 #Call the main function if this is the file being run.
